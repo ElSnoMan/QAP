48bd8063d382f7dca2ec12091eb0a2b04dc0b3ef
Unknown <ckcarlosdesign@gmail.com>
2018-06-27 22:01:20 -0600

    chromedriver.exe added and ready for first challenge

---
+++ Demo/Tests/Challenge.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Tests.Settings;
+
+namespace Tests
+{
+    [TestClass]
+    public class Challenge
+    {
+        IWebDriver Driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Driver = new ChromeDriver(Config.DRIVERPATH);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (Driver != null)
+                Driver.Quit();
+        }
+
+        [TestMethod]
+        public void StatsRoyaleChallenge()
+        {
+            // AUTOMATE THE CHALLENGE
+            // ======================
+            // 1. Go to statsroyale.com
+            // 2. Go to Popular Decks
+            // 3. Sort by Tournaments
+            // 4. Sort by Win Percentage
+            // 5. Assert that win percentage is 91.05
+
+            Driver.Navigate().GoToUrl("https://statsroyale.com");
+        }
+    }
+}
---
+++ Demo/Tests/Settings/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Tests.Settings
+{
+    public static class Config
+    {
+        public static string DRIVERPATH =
+            Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())
+                         .Parent.Parent.FullName, "drivers");
+    }
+}
